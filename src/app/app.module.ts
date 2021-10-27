80f8d80898c36c0322566eb2aa268495817aee44
salmarakha <61853942+salmarakha@users.noreply.github.com>
2021-10-27 19:11:17 +0200

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AuthInterceptor } from './services/auth.interceptor';
 
 // Angular Material imports
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { AuthInterceptor } from './services/auth.interceptor';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -29,6 +34,7 @@ import { AuthInterceptor } from './services/auth.interceptor';
     MatCardModule,
     MatInputModule,
     MatButtonModule,
+    MatToolbarModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
